a2ec569fc42eedf24106f5540f273199cad677f5
Laura Berge <lauraeberge@gmail.com>
2019-08-31 19:01:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  random_number = rand(7)
+  random_number = (rand(6) + 1)
   user_input = gets
   if user_input == random_number
     puts "You guessed the correct number"

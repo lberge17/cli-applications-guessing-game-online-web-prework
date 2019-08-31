8583404db47cffc67be8cba16757d266131c33ac
Laura Berge <lauraeberge@gmail.com>
2019-08-31 19:04:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,13 +1,13 @@
 # Code your solution here!
 def run_guessing_game
-  #random = (rand(6) + 1)
+  random = (rand(6) + 1)
   input = gets
-  if input == (rand(6) + 1)
+  if input == random
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
-  elsif input != (rand(6) + 1)
-    puts "Sorry! The computer guessed #{(rand(6) + 1)}."
+  elsif input != random
+    puts "Sorry! The computer guessed #{random}."
   end
 end
 
